de9bce728160b867b64d9a386aad7bcad4739b29
Ebenezer Arthur <arthurebenezer@aol.com>
2025-05-05 23:48:06 +0000

    refactor nav-header to handle app selection logic
    -  this also handles cases where no app is selected and the 'requests|  app logs' button throws an error because param.app is undefined

--- app/components/nav-header.tsx
+++ app/components/nav-header.tsx
@@ -1,11 +1,16 @@
-import { Link } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import clsx from "clsx";
 import { useLocation, useParams } from "react-router-dom";
+import type { loader } from "~/routes/app";
 
 export function NavHeader() {
+	const { apps } = useLoaderData<typeof loader>();
+	const appCount = apps.length;
+
 	const { pathname } = useLocation();
 
 	const params = useParams();
+	const appId = params.app;
 
 	const isRequests = pathname.includes("/requests");
 	const isLogs = pathname.includes("/logs");
@@ -24,27 +29,37 @@ export function NavHeader() {
 					className={clsx(
 						"flex text-sm font-mono border rounded-lg divide-x dark:(border-neutral-700 divide-neutral-700) overflow-hidden",
 						{
-							"!border-blue-500/70": isLogs,
-							"!border-rose-500/70": isRequests,
+							"!border-blue-500/70": isLogs && appCount > 0 && appId,
+							"!border-rose-500/70": isRequests && appCount > 0 && appId,
 						},
 					)}
 				>
-					<Link
-						className={clsx("bg-zinc-100 dark:bg-neutral-800 px-2 py-1", {
-							"!bg-rose-500 text-white": isRequests,
-						})}
-						to={`/app/${params.app}/requests`}
-					>
-						Requests
-					</Link>
-					<Link
-						className={clsx("bg-zinc-100 dark:bg-neutral-800 px-2 py-1", {
-							"!bg-blue-500 text-white": isLogs,
-						})}
-						to={`/app/${params.app}/logs`}
-					>
-						App Logs
-					</Link>
+					{appCount > 0 && appId ? (
+						<>
+							<Link
+								className={clsx("bg-zinc-100 dark:bg-neutral-800 px-2 py-1", {
+									"!bg-rose-500 text-white": isRequests,
+								})}
+								to={`/app/${appId}/requests`}
+							>
+								Requests
+							</Link>
+							<Link
+								className={clsx("bg-zinc-100 dark:bg-neutral-800 px-2 py-1", {
+									"!bg-blue-500 text-white": isLogs,
+								})}
+								to={`/app/${appId}/logs`}
+							>
+								App Logs
+							</Link>
+						</>
+					) : (
+						<span className="bg-zinc-100 dark:bg-neutral-800 px-2 py-1 text-gray-500">
+							{appCount === 0
+								? "Add an app to view logs"
+								: "Select an app to view logs"}
+						</span>
+					)}
 				</div>
 			</div>
 		</header>
